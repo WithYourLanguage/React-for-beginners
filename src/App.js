94844bffe9fd98386c6e4d9161e2c2cf7473636f
강 <ghang0501@gmail.com>
2023-05-17 21:38:36 +0900

    6.4 Cleanup function

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
-
-  useEffect(() => {
-    console.log("i run only once");
+function Hello() {
+  function byFn() {
+    console.log("디스트로이");
+  }
+  function hiFn() {
+    console.log("크리에이트");
+    return byFn;
+  }
+  useEffect(function () {
+    console.log("Hi (:");
+    return function () {
+      console.log("Bay ):");
+    };
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("나는 키워드와 카운터가 변화되면 발생되는 console.log()에요!");
-  }, [counter, keyword]);
+  return <h1>Hello</h1>;
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
